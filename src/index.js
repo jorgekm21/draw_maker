5c284a911c17fadc30233ccfaee0351eb89a437e
Jorge Kassabji <jorgekm21@gmail.com>
2021-08-05 14:31:41 -0400

    Styling

--- src/index.js
+++ src/index.js
@@ -11,6 +11,6 @@ require('./socket')(io)
 app.set('port', 3000)
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.listen(app.get('port'), () => {
+server.listen(app.get('port'), () => {
     console.log('Aplicacion esta corriendo en el puerto ' + app.get('port'))
 })
