7a6d9402315f8285ee9ea7f3ac54b4b1098cd103
Jorge Kassabji <jorgekm21@gmail.com>
2021-08-07 10:29:58 -0400

    Server prepare

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ const express = require('express')
 const http = require('http')
 const path = require('path')
 const socketio = require('socket.io')
+const cors = require('cors')
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 require('./socket')(io)
 
-app.set('port', 3000)
+app.set('port', process.env.PORT || 5000)
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(cors())
 
 server.listen(app.get('port'), () => {
     console.log('Aplicacion esta corriendo en el puerto ' + app.get('port'))
